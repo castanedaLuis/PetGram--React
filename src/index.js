429bfe7fb9821cb997d51795f3585461ee32d76f
castanedaLuis <castanedaLuis@github.com>
2023-01-04 16:55:31 -0600

    Linter, extensiones y deploy con Vercel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById('app'))
 root.render(
-    <React.StrictMode>
-      <h1>hola platzi petgram</h1>
-    </React.StrictMode>
-  );
+  <React.StrictMode>
+    <h1>hola platzi petgram</h1>
+  </React.StrictMode>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   output: {
     filename: 'app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template:'src/index.html'
+      template: 'src/index.html'
     })
   ],
   module: {
-        rules: [
-                {
-                        test: /\.js$/,
-                        exclude: /node_modules/,
-                        use: {
-                                loader: 'babel-loader',
-                                options: {
-                                        presets: [
-                                                '@babel/preset-env',
-                                                '@babel/preset-react'
-                                        ]
-                                }
-                        }
-                }
-        ]
-}
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
