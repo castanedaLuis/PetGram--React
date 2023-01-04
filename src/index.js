14c25d70313dcdcc24f579859946108dfc967dbf
castanedaLuis <castanedaLuis@github.com>
2023-01-04 13:35:32 -0600

    Instalación de React y Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('mi proyecto con react');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+    <React.StrictMode>
+      <h1>hola platzi petgram</h1>
+    </React.StrictMode>
+  );
