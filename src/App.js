8421e6f59e4e1ecd1676ebfad6753428f9cbc153
castanedaLuis <castanedaLuis@github.com>
2023-01-04 22:05:00 -0600

    Creando PhotoCard y usando react-icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {GlobalStyle} from './createGlobalStyle'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 function App() {
   return (
     <>
         <GlobalStyle/>
         <ListOfCategories />
+        <ListOfPhotoCards />
     </>
   )
 }
