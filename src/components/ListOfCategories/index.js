c9986143115616511a73ba69d580bbe5855f0368
castanedaLuis <castanedaLuis@github.com>
2023-01-05 16:15:12 -0600

    estilos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import{ Category }from '../Category'
-import{ List,Item }from './styles'
+import React, { useEffect, useState } from 'react'
+import { Category } from '../Category'
+import { List, Item } from './styles'
 import db from '../../../api/db.json'
 
-export const ListOfCategories=()=>{
+export const ListOfCategories = () => {
+    const [categories, setCategories] = useState(db.categories)
+    const [showFixed, setShowFixed] = useState(false)
 
-    return(
-    <List>
-        {
-            db.categories.map(category=>
-            <Item key={category.id}>
-                <Category {...category}/>
-            </Item>)
+    useEffect(() => {
+        console.log('fetch');
+    }, [])
+    useEffect(function () {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
         }
-    </List>
+
+        document.addEventListener('scroll', onScroll)
+
+        //Para limpiar el useEffect
+        return () => document.removeEventListener('scroll', onScroll)
+
+    }, [showFixed])
+
+    const listRender = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
+            {
+                categories.map(category =>
+                    <Item key={category.id}>
+                        <Category {...category} />
+                    </Item>)
+            }
+        </List>
+    )
+    return (
+        <>
+            {listRender()}
+            {showFixed && listRender(true)}
+        </>
     )
 }
