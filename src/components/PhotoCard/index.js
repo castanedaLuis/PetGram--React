2d69f0f8778a0d18897817ce7f78f06c992e9557
castanedaLuis <castanedaLuis@github.com>
2023-01-06 15:40:18 -0600

    Custom Hooks: useNearScreen y useLocalStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,53 +1,22 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
-    const element = useRef(null)
-    const [show, setShow] = useState(false)
+    const [show, element] = useNearScreen()
 
     //like-12 
     const key = `like-${id}`
-
-    //like-12 --> regresa true o flase si encuentra algo con la key que le pasamos 
-    const [liked, setLiked] = useState(() => {
-        try {
-            const like = window.localStorage.getItem(key)
-            console.log(JSON.parse(like));
-            return JSON.parse(like)
-        } catch (e) {
-            return false
-        }
-    })
-
+    //regresa true o flase si encuentra algo con la key que le pasamos 
+    const [liked, setLiked] = useLocalStorage(key, false)
     const Icon = liked ? MdFavorite : MdFavoriteBorder
 
-    const setLocalStorage = value => {
-        try {
-            window.localStorage.setItem(key, value)
-            setLiked(value)
-        } catch (e) {
-            console.error(e)
-        }
-    }
 
-    useEffect(function () {
-        Promise.resolve(
-            typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer'))
-            .then(() => {
-                const observer = new window.IntersectionObserver(function (entries) {
-                    const { isIntersecting } = entries[0]
-                    if (isIntersecting) {
-                        setShow(true)
-                        observer.disconnect()
-                    }
-                })
-                observer.observe(element.current)
-            })
-    }, [element])
 
     return (
         <Article ref={element}>
@@ -58,7 +27,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                         <ImgWrapper>
                             <Img src={src} />
                         </ImgWrapper></a>
-                    <Button onClick={() => setLocalStorage(!liked)}>
+                    <Button onClick={() => setLiked(!liked)}>
                         <Icon size='32px' />
                         {likes}likes!
                     </Button>
