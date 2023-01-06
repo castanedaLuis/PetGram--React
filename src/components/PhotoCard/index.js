f32b85f3fe222e8f98ec4d6f57e9902cbaad41bc
castanedaLuis <castanedaLuis@github.com>
2023-01-06 01:24:01 -0600

    Uso de polyfill de Intersection Observer e imports dinámicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,4 @@
-import React , {useRef, useEffect, useState} from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
@@ -8,33 +8,38 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     const element = useRef(null)
     const [show, setShow] = useState(false)
 
+
+
     useEffect(function () {
-        const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0]
-          console.log(isIntersecting)
-          if (isIntersecting) {
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(element.current)
-      }, [element])
-      
-    return (
-            <Article ref={element}>
-                {
-                    show &&
-                    <>
-                        <a href={`/detail/${id}`}>
-                        <ImgWrapper>
-                        <Img src={src} />
-                        </ImgWrapper></a>
-                        <Button>
-                            <MdFavoriteBorder size='32px' />
-                                {likes}likes!
-                        </Button>
-                    </>
+        Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer'))
+            .then(() => {
+            const observer = new window.IntersectionObserver(function (entries) {
+                const { isIntersecting } = entries[0]
+                if (isIntersecting) {
+                    setShow(true)
+                    observer.disconnect()
                 }
-            </Article>
-            )
+            })
+            observer.observe(element.current)
+        })
+    }, [element])
+
+    return (
+        <Article ref={element}>
+            {
+                show &&
+                <>
+                    <a href={`/detail/${id}`}>
+                        <ImgWrapper>
+                            <Img src={src} />
+                        </ImgWrapper></a>
+                    <Button>
+                        <MdFavoriteBorder size='32px' />
+                        {likes}likes!
+                    </Button>
+                </>
+            }
+        </Article>
+    )
 }
